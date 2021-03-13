3c64ae4c07575ae0e98f5d3d647432f4e27190ae
EC2 Default User <ec2-user@ip-172-31-47-22.ec2.internal>
2021-03-13 12:57:23 +0000

    [Update]ルートの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,22 @@ class BooksController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def_book_params
+  params.require(:book).permit(:title, :body)
 end
